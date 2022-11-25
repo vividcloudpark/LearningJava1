1161e07bce2a832bca253dd3e3f0ef93bc4f8f95
cloudpark <vividxenon@gmail.com>
2022-11-25 17:38:16 +0900

    JDBC

--- src/main/java/hello/spring/SpringConfig.java
+++ src/main/java/hello/spring/SpringConfig.java
@@ -1,8 +1,12 @@
 package hello.spring;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import hello.spring.repository.JdbcMemberRepository;
 import hello.spring.repository.MemberRepository;
 import hello.spring.repository.MemoryMemberRepository;
 import hello.spring.service.MemberService;
@@ -10,9 +14,17 @@ import hello.spring.service.MemberService;
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+    
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
     @Bean
