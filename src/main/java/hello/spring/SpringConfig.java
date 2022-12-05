e76743a46ec10317ee44b42c7972c5f4c28efb49
cloud.park <vividxenon@gmail.com>
2022-12-05 21:20:57 +0900

    JPA 완료

--- src/main/java/hello/spring/SpringConfig.java
+++ src/main/java/hello/spring/SpringConfig.java
@@ -1,31 +1,33 @@
 package hello.spring;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
-import hello.spring.repository.JdbcTemplateMemberRepository;
+import hello.spring.repository.JpaMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import hello.spring.repository.JdbcMemberRepository;
 import hello.spring.repository.MemberRepository;
 import hello.spring.service.MemberService;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
-    
+//    private DataSource dataSource;
+    private EntityManager em;
+
     @Autowired
-    public SpringConfig(DataSource dataSource){
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em){
+        this.em = em;
     }
 
     @Bean
     public MemberRepository memberRepository(){
         // return new MemoryMemberRepository();
         //  return new JdbcMemberRepository(dataSource);
-        return  new JdbcTemplateMemberRepository(dataSource);
+//        return  new JdbcTemplateMemberRepository(dataSource);
+        return  new JpaMemberRepository(em);
     }
 
     @Bean
--- src/main/java/hello/spring/domain/Member.java
+++ src/main/java/hello/spring/domain/Member.java
@@ -1,7 +1,14 @@
 package hello.spring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+//    @Column(name = "username")
     private String name;
 
     public Long getId() {
